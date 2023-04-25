f759d2b52e0d2557f05afccdff7aa78b4f59070b
Pablo Elgueta <pablo.elguetav@gmail.com>
2023-04-24 22:06:38 -0400

    PEV: Se actualizan rrss y se agranda imagen de servicios

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
+import { FaYoutube } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-        <div>
-            <FaFacebookF/>
-        </div>
-        <div>
-            <BsInstagram/>
-        </div>
+      
+      <a href='https://www.facebook.com/100083496823250/videos/907978073650590' target='_blank' rel='noreferrer'><div><FaYoutube /></div></a>
+        
+      <a href='https://www.facebook.com/profile.php?id=100041521071834' target='_blank' rel='noreferrer'><div><FaFacebookF/></div></a> 
+       
+      <a href='https://www.instagram.com/autofibravaldivia/'  target='_blank' rel='noreferrer'><div><BsInstagram/></div></a>
+      
     </div>
   )
 }
